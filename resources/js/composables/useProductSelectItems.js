95788538b9f1ab6f3493690c03bac5b360ba10b9
breezyx28 <mohamedx.28@gmail.com>
2026-07-17 00:08:21 +0200

    fixed purchase products list not showing

--- resources/js/composables/useProductSelectItems.js
+++ resources/js/composables/useProductSelectItems.js
@@ -1,11 +1,11 @@
-import { computed, unref } from 'vue';
+import { computed, toValue } from 'vue';
 import { useTrans } from '@/composables/useTrans';
 
 export const PRODUCT_SELECT_FILTER_FIELDS = ['label', 'sku'];
 
 /** Map API product options to USelectMenu items searchable by name and SKU. */
 export function mapProductSelectItems(products) {
-    return (products ?? []).map((p) => ({
+    return (Array.isArray(products) ? products : []).map((p) => ({
         label: p.name,
         value: p.id,
         sku: p.sku ?? '',
@@ -19,7 +19,7 @@ export function useProductSelectItems(productOptionsSource) {
     const { t } = useTrans();
 
     const productItems = computed(() =>
-        mapProductSelectItems(unref(productOptionsSource)),
+        mapProductSelectItems(toValue(productOptionsSource)),
     );
 
     const productSelectAttrs = computed(() => ({
